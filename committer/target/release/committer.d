c20a76f2402147249752ae39ab2cde87c7a6ddd6
Nolan Neff <nolanneff@gmail.com>
2026-01-12 15:22:04 -0700

    chore: Restructure project for better org and clarity
    - Moves all files in the `committer` directory to root.
    - Removes now redundant `committer` directory.

--- committer/target/release/committer.d
+++
@@ -1,1 +0,0 @@
-/mnt/shared/Coding\ Storage/Projects\ -\ 2025/commiter/committer/target/release/committer: /mnt/shared/Coding\ Storage/Projects\ -\ 2025/commiter/committer/src/main.rs
--- committer/target/release/deps/committer-282b583f52eac7d1.d
+++
@@ -1,5 +0,0 @@
-/workspace/committer/target/release/deps/committer-282b583f52eac7d1: src/main.rs
-
-/workspace/committer/target/release/deps/committer-282b583f52eac7d1.d: src/main.rs
-
-src/main.rs:
